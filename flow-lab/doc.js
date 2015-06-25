007e77513b194f2e7708f7e19bb4d24e4fd7d079
Alison <alisontong@hotmail.com>
2015-06-24 23:21:56 -0700

    added flow-lab

---
+++ flow-lab/doc.js
@@ -0,0 +1,15 @@
+var numberVerses = prompt("how many beers");
+var newNumber = var numberVerses -1
+for ( var numberVerses = 5; numberVerses<=5 ; numberVerses--){
+  if (numberVerses > 1){
+  console.log(numberVerses + " bottles of beer on the wall");
+  console.log(numberVerses + " bottles of beer!");
+  console.log("Take one down and pass it around,");
+  console.log(newNumber + "bottles on the wall");
+  }else (numberVerses > 0){
+   console.log(numberVerses + " bottle of beer on the wall");
+   console.log(numberVerses + " bottle of beer!");
+   console.log("Take one down and pass it around,");
+   console.log("no more")  
+}
+}
